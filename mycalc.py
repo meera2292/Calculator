480189adf3ab0cabeeb9393305007d7384c0f303
Meera Antony <meera2292@gmail.com>
2026-07-24 09:39:24 +0530

    my calculator commit

---
+++ mycalc.py
@@ -0,0 +1,9 @@
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+operation=input("Enter operation (+, -, *, /): ")
+match operation:
+    case "+":   print(num1 + num2)
+    case "-":   print(num1 - num2)
+    case "*":   print(num1 * num2)
+    case "/":   print(num1 / num2)              
+    case _:   print("Invalid operation")
